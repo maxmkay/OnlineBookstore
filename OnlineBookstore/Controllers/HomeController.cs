d17d7b358268f17728d95af2fbe888bdef636074
maxmkay <13maxmcgrath@gmail.com>
2022-02-16 22:44:13 -0700

    Add project files.

---
+++ OnlineBookstore/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineBookstore.Models;
+using OnlineBookstore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookStoreRepository repo;
+
+        public HomeController (IBookStoreRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index(int pageNum = 1)
+        {
+
+            int pageSize = 10;
+
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(p => p.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+           
+
+            return View(x);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OnlineBookstore/Models/EFBookstoreRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBookstore.Models
+{
+    public class EFBookstoreRepository : IBookStoreRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBookstoreRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Books> Books => context.Books;
+    }
+}
